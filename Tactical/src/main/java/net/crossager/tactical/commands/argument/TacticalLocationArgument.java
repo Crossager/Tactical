39d63406a5682f5f20f140272fa166002f05e672
Crossager <thor@lmnet.dk>
2024-03-30 20:49:45 +0100

    Remove brigadier implementation

--- Tactical/src/main/java/net/crossager/tactical/commands/argument/TacticalLocationArgument.java
+++ Tactical/src/main/java/net/crossager/tactical/commands/argument/TacticalLocationArgument.java
@@ -1,45 +1,16 @@
 package net.crossager.tactical.commands.argument;
 
-import com.mojang.brigadier.StringReader;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.crossager.tactical.api.commands.context.TacticalCommandArgumentActionContext;
 import net.crossager.tactical.api.commands.context.TacticalCommandArgumentTabCompletionContext;
 import net.crossager.tactical.api.util.TacticalUtils;
-import net.minecraft.commands.arguments.coordinates.ArgumentVectorPosition;
-import net.minecraft.util.MathHelper;
-import net.minecraft.world.phys.Vec2F;
-import net.minecraft.world.phys.Vec3D;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TacticalLocationArgument extends TacticalCommandArgumentBase {
-    private static final Field bField;
-    private static final Field cField;
-    private static final Field dField;
-
-    static {
-        try {
-            bField = ArgumentVectorPosition.class.getDeclaredField("b");
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            cField = ArgumentVectorPosition.class.getDeclaredField("c");
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            dField = ArgumentVectorPosition.class.getDeclaredField("d");
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     private final String formatString;
 
     public TacticalLocationArgument(String name, int decimals) {
@@ -82,48 +53,6 @@ public class TacticalLocationArgument extends TacticalCommandArgumentBase {
         Location relative = context.sender() instanceof Entity entity ?
                 entity.getLocation() :
                 new Location(null, 0, 0, 0);
-        if (context.arguments().get(0).charAt(0) == '^') {
-            // tbf i dont know any of what it does, its just mojangs own implementation of local positions.
-            ArgumentVectorPosition vectorPosition;
-            try {
-                vectorPosition = ArgumentVectorPosition.a(new StringReader(context.argumentString()));
-            } catch (CommandSyntaxException e) {
-                throw new RuntimeException(e);
-            }
-            double d;
-            double c;
-            double b;
-            try {
-                d = (double) dField.get(vectorPosition);
-                c = (double) cField.get(vectorPosition);
-                b = (double) bField.get(vectorPosition);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-            Vec2F var1 = new Vec2F(relative.getYaw(), relative.getPitch());
-            Vec3D var2 = new Vec3D(relative.getX(), relative.getY(), relative.getZ());
-            float var3 = MathHelper.b((var1.j + 90.0F) * 0.017453292F);
-            float var4 = MathHelper.a((var1.j + 90.0F) * 0.017453292F);
-            float var5 = MathHelper.b(-var1.i * 0.017453292F);
-            float var6 = MathHelper.a(-var1.i * 0.017453292F);
-            float var7 = MathHelper.b((-var1.i + 90.0F) * 0.017453292F);
-            float var8 = MathHelper.a((-var1.i + 90.0F) * 0.017453292F);
-            Vec3D var9 = new Vec3D(var3 * var5, var6, var4 * var5);
-            Vec3D var10 = new Vec3D(var3 * var7, var8, var4 * var7);
-            Vec3D var11 = var9.c(var10).a(-1.0);
-            double var12 = var9.c * d + var10.c * c + var11.c * b;
-            double var14 = var9.d * d + var10.d * c + var11.d * b;
-            double var16 = var9.e * d + var10.e * c + var11.e * b;
-            Vec3D result = new Vec3D(var2.c + var12, var2.d + var14, var2.e + var16);
-            return new Location(
-                    relative.getWorld(),
-                    result.c,
-                    result.d,
-                    result.e,
-                    relative.getYaw(),
-                    relative.getPitch()
-            );
-        }
         String xs = context.arguments().get(0);
         String ys = context.arguments().get(1);
         String zs = context.arguments().get(2);
