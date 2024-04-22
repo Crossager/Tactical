a8309908ccaffcc0145c1a473ae1d53d53136518
Crossager <thor@lmnet.dk>
2024-04-22 19:29:20 +0200

    Cache enum values

---
+++ Tactical/src/main/java/net/crossager/tactical/util/CachedEnumValues.java
@@ -0,0 +1,7 @@
+package net.crossager.tactical.util;
+
+import org.bukkit.inventory.EquipmentSlot;
+
+public class CachedEnumValues {
+    public static final EquipmentSlot[] EQUIPMENT_SLOT = EquipmentSlot.values();
+}
