1a7c7fb7a2da2f59613a62c43189cac4269f36c6
Crossager <thor@lmnet.dk>
2024-04-28 14:23:28 +0200

    Player NPC metadata enums

---
+++ TacticalNPC/src/main/java/net/crossager/tactical/api/npc/TacticalPlayerSkinParts.java
@@ -0,0 +1,11 @@
+package net.crossager.tactical.api.npc;
+
+public enum TacticalPlayerSkinParts {
+    CAPE,
+    JACKET,
+    LEFT_SLEEVE,
+    RIGHT_SLEEVE,
+    LEFT_PANTS_LEG,
+    RIGHT_PANTS_LEG,
+    HAT
+}
