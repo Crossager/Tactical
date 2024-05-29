4767eb9e3b36a1cdffadbfc555f2c6fcfee1d692
Crossager <thor@lmnet.dk>
2024-05-29 21:41:41 +0200

    Remove comment

--- Tactical/src/main/java/net/crossager/tactical/gui/GUIReflection.java
+++ Tactical/src/main/java/net/crossager/tactical/gui/GUIReflection.java
@@ -5,6 +5,5 @@ import net.crossager.tactical.util.reflect.DynamicReflection;
 import net.crossager.tactical.util.reflect.MinecraftClasses;
 
 public class GUIReflection {
-//    public static final Gson CHAT_MESSAGE_GSON = DynamicReflection.getField(MinecraftClasses.getChatSerializerClass(), Gson.class).read(null);
     public static final MethodInvoker<Integer> NEXT_CONTAINER_COUNTER = DynamicReflection.getMethodByReturnTypeAndArgs(MinecraftClasses.getChatSerializerClass(), int.class);
 }
